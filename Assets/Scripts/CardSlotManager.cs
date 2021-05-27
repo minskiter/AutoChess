a6728e0fd1465580a23ca369f5ee36df2a8d15c6
ElizzF <838331058@qq.com>
2021-05-27 10:22:24 +0800

    [update]更新卡牌点击逻辑

--- Assets/Scripts/CardSlotManager.cs
+++ Assets/Scripts/CardSlotManager.cs
@@ -3,41 +3,80 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
-public class CardSlotManager : MonoBehaviour
+using UnityEngine.EventSystems;
+public class CardSlotManager : MonoBehaviour, IPointerClickHandler
 {
-    private Canvas canvas;
-    private Object cardSlot;
     private Image card;
-    private int[,] cards;  //卡牌数组   
+    public Image[] cards;  
     private int maxSize = 5; //卡槽容量
+
+    private bool isClick = false;  //标记是否点击卡牌了
+
+    //点击卡牌的坐标
+    private float x;
+    private float y;
+    private float z;
+    //点击卡牌的id
+    private int id = -1;
+
    
     void Start()
     {
+        cards = new Image[maxSize]; 
         InitCardSlot();
     }
 
     //初始化卡槽
     void InitCardSlot() {
-        for(int i = 1; i <= 5; i++) {
+        for(int i = 0; i < maxSize; i++) {
             GameObject obj = new GameObject();
+            obj.name = "bighero" + (i + 1);
             obj.transform.parent = this.transform;
             card = obj.AddComponent<Image>();
-            card.sprite = Resources.Load("heros/bighero" + i, typeof(Sprite)) as Sprite;
+            card.sprite = Resources.Load("heros/bighero" + (i + 1), typeof(Sprite)) as Sprite;
             card.transform.localScale = new Vector3(3f,3f,1f);
+            cards[i] = card;
         }
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        GameObject obj = null;
+        PointerEventData eventDataCurrentPosition = new PointerEventData(EventSystem.current);
+        eventDataCurrentPosition.position = new Vector2
+        (
+            Input.mousePosition.x, Input.mousePosition.y
+        );
+        List<RaycastResult> results = new List<RaycastResult>();
+        EventSystem.current.RaycastAll(eventDataCurrentPosition, results);
+        if(results.Count > 0) {
+            obj = results[0].gameObject;  //获取点击卡牌属性
+        }
+  
+        //点击突出逻辑
+        if(id == -1 || id == (int)obj.name[obj.name.Length - 1]) {
+            id = (int)obj.name[obj.name.Length - 1];
+            if(isClick == false) {
+                x = obj.transform.position.x;
+                y = obj.transform.position.y;
+                z = obj.transform.position.z;
+                obj.transform.localScale = new Vector3(3.5f, 3.5f, 1f);
     
+                obj.transform.position = new Vector3(x, y + 10f, z);
+                isClick = true;
+            }
+            else {
+                obj.transform.localScale = new Vector3(3f, 3f, 1f);
+                obj.transform.position = new Vector3(x, y, z);
+                isClick = false;
+                id = -1;
+            }
+        }
+       
         
-
-
-
-        // card = Resources.Load("card") as GameObject;
-        // GameObject go = Instantiate(card) as GameObject;
-        // go.transform.parent = this.transform;
     }
 
     
-
-
     void Update()
     {
         
