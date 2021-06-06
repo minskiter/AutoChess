f81ccca948741994ac8d99c614f278048e9f4ca2
minskiter <minskiter@qq.com>
2021-06-06 23:31:43 +0800

    [update] update LF

--- Assets/Scripts/Data/CsvLoader.cs
+++ Assets/Scripts/Data/CsvLoader.cs
@@ -12,14 +12,14 @@ static public class CsvLoader
     {
         //var lines = File.ReadLines(Path.Combine(Application.dataPath, path));
         Debug.Log(path);
-        var lines = (Resources.Load(path) as TextAsset)?.text.Split(new []{Environment.NewLine},StringSplitOptions.None).ToList();
+        var lines = (Resources.Load(path) as TextAsset)?.text.Split('\n').ToList();
         Debug.Log(lines);
         var headers = lines.First().Split(',');
         var table = new DataTable();
         foreach (var header in headers){
             table.Columns.Add(header);
         }
-        lines = (Resources.Load(path) as TextAsset)? .text.Split(new[] { Environment.NewLine }, StringSplitOptions.None).ToList();
+        lines = (Resources.Load(path) as TextAsset)? .text.Split('\n').ToList();
         //lines = File.ReadLines(Path.Combine(Application.dataPath, path));
         foreach (var line in lines.Skip(1))
         {   
